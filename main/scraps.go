790473375973fce7f30d64fc46c01311f996021f
Graham Kerr <graham.kerr@revolut.com>
2019-05-24 00:32:33 +0200

    change gitignore

--- main/scraps.go
+++ main/scraps.go
@@ -1,1 +1,19 @@
 package main
+
+//func getNextMaxLengths(liveCells CellTable) []int {
+//	// limit new board size to current live plus one for each edge
+//	xLength := len(liveCells) + 2
+//	yLength := getLongestLen(liveCells) + 2
+//
+//	return []int{xLength, yLength}
+//}
+
+// used to get the longest Y slice length when creating new board
+//func getLongestLen(board CellTable) (length int) {
+//	for _, col := range board {
+//		if len(col) > length {
+//			length = len(col)
+//		}
+//	}
+//	return
+//}
